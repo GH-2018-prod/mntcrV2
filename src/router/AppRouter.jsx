4267e48f0796dd9a96ae307cacffe85de6a61084
GH-2018-prod <maancuza@gmail.com>
2023-02-08 06:20:22 -0600

    "app crashed. updating...."

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { LogInPage } from "../auth"
-import { Faqs, Home, TeachersNativos, Testimonios } from "../pages"
-import { Classes } from "../pages/pages/Classes"
-
+import { Faqs, Home, TeachersNativos, Testimonios, Classes } from "../pages"
 
 export const AppRouter = () => {
 
