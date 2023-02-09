1ef7f34adae22af2a9a1cd3f50a52f7ea29595d8
GH-2018-prod <maancuza@gmail.com>
2023-02-08 19:40:03 -0600

    uppdating more files

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import { LogInPage } from "../auth"
-import { Faqs, TeachersNativos, Testimonios, Classes } from "../pages"
 import { Home } from "../pages/pages/Home"
 
 
@@ -12,11 +11,6 @@ export const AppRouter = () => {
     <Routes>
       <Route path="/auth/*" element={ <LogInPage />}/>
       <Route path="/" element={ <Home/>}/>
-      <Route path="/teachers" element={ <TeachersNativos />}/>
-      <Route path="/clases" element={ <Classes />}/>
-      <Route path="/faqs" element={ <Faqs />}/>
-      <Route path="/testimonios" element={ <Testimonios />}/>
-
     </Routes>
     
   )
