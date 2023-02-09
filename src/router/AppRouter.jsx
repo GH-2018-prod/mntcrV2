8aacbce3405fcae98f425b93fbe938b1f5d945ae
GH-2018-prod <maancuza@gmail.com>
2023-02-08 20:31:49 -0600

    redefining route for / Home

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { LogInPage } from "../auth"
-<<<<<<< HEAD
-import { Faqs, TeachersNativos, Testimonios, Classes } from "../pages"
-=======
->>>>>>> 1ef7f34adae22af2a9a1cd3f50a52f7ea29595d8
+//import { Faqs, TeachersNativos, Testimonios, Classes } from "../pages"
 import { Home } from "../pages/pages/Home"
 
 
@@ -14,7 +11,7 @@ export const AppRouter = () => {
 
     <Routes>
       <Route path="/auth/*" element={ <LogInPage />}/>
-      <Route path="/" element={ <Home/>}/>
+      <Route path="/*" element={ <Home/>}/>
     </Routes>
     
   )
